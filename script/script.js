b25fb85f6bc9ff5caaaf8a14f83a3ecdd064b403
Simon Simeonov <simeon.p.simeonov92@gmail.com>
2023-10-25 00:05:58 +0300

    add validation for email

--- script/script.js
+++ script/script.js
@@ -1,1 +1,13 @@
-// write your JavaScript here
+let submitEmail = document.querySelector('.subscribe-wrapper button');
+let emailInput = document.querySelector('.subscribe-wrapper-form input');
+
+submitEmail.addEventListener('click', (event) => {
+   event.preventDefault();
+   let mail = emailInput.value;
+   let mailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+   if (!mail.match(mailRegex)) {
+      alert('The email is invalid ...');
+   } else {
+      alert('Thanks for subscribing !');
+   }
+});
